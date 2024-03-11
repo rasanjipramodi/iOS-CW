99ea6180f080a36a3af7923c42f38bcad9082f81
Rasanji Pramodi <127526750+rasanjipramodi@users.noreply.github.com>
2024-03-11 14:46:38 +0530

    Initial Commit

---
+++ Fashion Fixx/Fashion_FixxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fashion_FixxApp.swift
+//  Fashion Fixx
+//
+//  Created by Tharaka Mohotti on 2024-03-11.
+//
+
+import SwiftUI
+
+@main
+struct Fashion_FixxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
